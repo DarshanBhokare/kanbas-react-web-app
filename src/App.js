2b813cdfc5e573a7e49247927d55e38dd40ac9d7
DarshanBhokare <bhokare.d@northeastern.edu>
2023-10-20 01:30:18 -0400

    a3

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import "./App.css";
-import Labs from "./Labs";
-import HelloWorld from "./Labs/a3/HelloWorld";
-import Kanbas from "./Kanbas";
-import { HashRouter } from "react-router-dom";
-import { Routes, Route, Navigate } from "react-router-dom";
+import './App.css';
+import Labs from './Labs';
+import HelloWorld from './Labs/a3/HelloWorld';
+import Kanbas from './Kanbas';
+import { HashRouter } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   return (
     <HashRouter>
-    {/* <div> */}
       <Routes>
-        <Route path="/" element={<Navigate to="/Labs"/>} />
+        <Route path="/" element={<Navigate to="/Labs" />} />
         <Route path="/hello" element={<HelloWorld />} />
-        <Route path="/Labs/*"   element={<Labs/>}/>
-        <Route path="/Kanbas/*" element={<Kanbas/>}/>
+        <Route path="/Labs/*" element={<Labs />} />
+        <Route path="/Kanbas/*" element={<Kanbas />} />
       </Routes>
-    {/* </div> */}
     </HashRouter>
   );
 }
--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -1,8 +1,8 @@
-import KanbasNavigation from "./KanbasNavigation";
-import { Routes, Route, Navigate } from "react-router";
-import Dashboard from "./Dashboard";
-import Courses from "./Courses";
-import "./styles.css";
+import KanbasNavigation from './KanbasNavigation';
+import { Routes, Route, Navigate } from 'react-router';
+import Dashboard from './Dashboard';
+import Courses from './Courses';
+import './styles.css';
 
 function Kanbas() {
   return (
@@ -10,15 +10,12 @@ function Kanbas() {
       <div className="wd-kanbas-navigation">
         <KanbasNavigation />
       </div>
-      {/* <div> */}
-        <Routes>
-          <Route path="/" element={<Navigate to="Dashboard" />} />
-          <Route path="Account" element={<h1>Account</h1>} />
-          <Route path="Dashboard" element={<Dashboard />} />
-          <Route path="Courses" element={<Dashboard />} />
-          <Route path="Courses/:courseId/*" element={<Courses />} />
-        </Routes>
-      {/* </div> */}
+      <Routes>
+        <Route path="/" element={<Navigate to="Dashboard" />} />
+        <Route path="Account" element={<h1>Account</h1>} />
+        <Route path="Dashboard" element={<Dashboard />} />
+        <Route path="Courses/:courseId/*" element={<Courses />} />
+      </Routes>
     </div>
   );
 }
